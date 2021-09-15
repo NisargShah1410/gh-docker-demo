bfafa6f7c15f56bab2f2d193ede4fbfc46d97937
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-09-15 11:35:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-echo "Hello $1
-profile=$(date)
-echo "::set-output name=profile::$profile"
+echo "Hello $1"
+$profile="dotnet"
+Write-Output "::set-output name=profile::$profile"
 time=$(date)
 echo "::set-output name=time::$time"
